aed0e02dad06d61d0736ac8ededed991a7d5826d
Anushree Agrawal <anushree.agrawal27@gmail.com>
2016-09-21 17:11:08 -0400

    Redo

--- public/runMatlab.sh
+++ public/runMatlab.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#scp variables/${1}.txt admin@130.132.20.134:~/parameters
+scp variables/${1}.txt admin@130.132.20.134:~/parameters
 
-ssh admin@130.132.20.134 /Applications/MATLAB_R2015a.app/bin/matlab -nodesktop -noFigureWindows -nosplash -nodisplay -r "script_runSimCommLine\(\'/Users/admin/parameters/20.txt\'\)"
+ssh admin@130.132.20.134 /Applications/MATLAB_R2015a.app/bin/matlab -nodesktop -noFigureWindows -nosplash -nodisplay -r "script_runSimCommLine\(\'/Users/admin/parameters/${1}.txt\'\)"
 
-scp admin@130.132.20.134:~/mfm_ldep_web/output_file.txt results/20.txt
+scp admin@130.132.20.134:~/mfm_ldep_web/output_file.txt results/${1}.txt
 
 php ../artisan finishJob ${1}
 
